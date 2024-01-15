2f09f9a0d607440122688c1c2f3d7c6297728724
Роман Владиславович Серебряков <rvs@ge74.ru>
2024-01-15 14:54:46 +0500

    add_controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Category\CreateController;
+use App\Http\Controllers\Category\DeleteController;
+use App\Http\Controllers\Category\EditController;
+use App\Http\Controllers\Category\IndexController as CategoryIndexController;
+use App\Http\Controllers\Category\ShowController;
+use App\Http\Controllers\Category\StoreController;
+use App\Http\Controllers\Category\UpdateController;
 use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +23,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class)->name('main.index');
 
+Route::group(['prefix' => 'categories'], function() {
+
+    Route::get('/', CategoryIndexController::class)->name('category.index');
+    Route::get('/create', CreateController::class)->name('category.create');
+    Route::post('/', StoreController::class)->name('category.store');
+    Route::get('/{category}/edit', EditController::class)->name('category.edit');
+    Route::get('/{category}', ShowController::class)->name('category.show');
+    Route::patch('/', UpdateController::class)->name('category.update');
+    Route::delete('/{category}', DeleteController::class)->name('category.delete');
+
+});
